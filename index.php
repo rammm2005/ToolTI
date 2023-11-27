f6d7b361d9c5b9104c2a74017f0416a814b7be51
rammm2005 <ditarama985@gmail.com>
2023-11-27 12:51:58 +0700

    fix link

--- index.php
+++ index.php
@@ -1,2 +1,27 @@
 <?php
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="apple-touch-icon" href="frontend/images/logo.png" sizes="180x180">
+    <link rel="icon" type="image/png" href="frontend/images/logo.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="frontend/images/logo.png" sizes="16x16">
+    <link rel="stylesheet" href="frontend/components/css/core.css">
+    <title>IT Tool</title>
+</head>
+<body>
+
+    <div class="bg-red-500">
+        peler
+    </div>
+
+
+<script src="https://cdn.tailwindcss.com"></script>
+<script src="frontend/components/javascript/jquery.3.1.js"></script>
+<script src="frontend/components/javascript/script.js"></script>
+</body>
+</html>
