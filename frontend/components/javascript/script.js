b5a9c27d2d786e2239b900f20fc8202b8054a75d
rammm2005 <ditarama985@gmail.com>
2023-11-27 12:20:31 +0700

    tailwind theme setup

---
+++ frontend/components/javascript/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    // tailwind theme setup
+    tailwind config = {
+        theme : {
+            extend :{
+                fontFamily : {
+                    sans : ['Nunito'],
+                },
+                color : {
+                    core: "#da373d",
+                }
+            }
+        }
+    }
+
+});
